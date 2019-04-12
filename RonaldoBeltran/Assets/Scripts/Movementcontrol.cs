03eb25f44ad3be000839dc0a18a4d768825bb6d8
iTzSlower <iTzSlowerdtc@hotmail.com>
2019-04-11 22:03:59 -0500

    Clase movimiento

---
+++ RonaldoBeltran/Assets/Scripts/Movementcontrol.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movementcontrol : MonoBehaviour
+{
+
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //Vector3 tempVector = Vector3.zero;
+        //tempVector.z = speed;
+        //gameObject.transform.position += tempVector * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            Vector3 tempVector = Vector3.zero;
+            tempVector.z = speed;
+            gameObject.transform.position += tempVector * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 tempVector = Vector3.zero;
+            tempVector.z = -speed;
+            gameObject.transform.position += tempVector * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 tempVector = Vector3.zero;
+            tempVector.x = -speed;
+            gameObject.transform.position += tempVector * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 tempVector = Vector3.zero;
+            tempVector.x = speed;
+            gameObject.transform.position += tempVector * Time.deltaTime;
+        }
+    }
+}
