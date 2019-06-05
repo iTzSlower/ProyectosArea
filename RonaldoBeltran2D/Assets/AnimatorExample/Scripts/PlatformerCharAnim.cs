e9c78309e17c9e6e770f3dafadbd80953ae93e45
iTzSlower <iTzSlowerdtc@hotmail.com>
2019-06-04 22:11:53 -0500

    animaciones y movimiento

---
+++ RonaldoBeltran2D/Assets/AnimatorExample/Scripts/PlatformerCharAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerCharAnim : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+    public float moveSpeed;
+    public float JumpForce = 15;
+    float gravity = 10;
+    float vertSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    public void ControllerFixedUpdate ()
+    {
+        Vector2 horizontal = Input.GetAxisRaw("Horizontal") * Vector2.right;
+        Vector2 vertical = Vector2.up * vertSpeed * Time.fixedDeltaTime;
+    }
+}
---
+++ RonaldoBeltran2D/Assets/AnimatorExample/Scripts/TopdownCharAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopdownCharAnim : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+    public float moveSpeed = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    public void ControllerFixedUpdate()
+    {
+        Vector2 horizontal = Input.GetAxisRaw("Horizontal") * Vector2.right;
+        Vector2 vertical = Input.GetAxisRaw("Vertical") * Vector2.right;
+    }
+}
