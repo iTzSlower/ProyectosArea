aca0bca2f9c3ee7da60b6cadc44f2eda44792b4b
iTzSlower <iTzSlowerdtc@hotmail.com>
2019-06-11 22:04:17 -0500

    me falta pero me pongo al dia :3

--- RonaldoBeltran3D/Assets/Scripts/CamSprit.cs
+++ RonaldoBeltran3D/Assets/Scripts/CamSprit.cs
@@ -6,30 +6,32 @@ public class CamSprit : MonoBehaviour
 {
     public MovScript movingTarget;
     public Vector3 distanceFromTarget;
-    Vector3 distancePoint { get { return movingTarget.position + movingTarget.EqualizeWithLocal (distanceFromTarget); } }
+    Vector3 distancePoint { get { return movingTarget.position + movingTarget.EqualizeWithLocal(distanceFromTarget); } }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        if (movingTarget) {
+        if (movingTarget)
+        {
             transform.position = distancePoint;
-            transform.rotation = movingTarget.transform.rotation;
+            //transform.rotation = movingTarget.transform.rotation;
+            transform.LookAt(movingTarget.transform);
         }
     }
 
-    void OnDrawGizmos ()
+    void OnDrawGizmos()
     {
         if (movingTarget)
         {
             Gizmos.DrawWireSphere(distancePoint, 0.25f);
             Gizmos.color = Color.magenta;
-            Gizmos.DrawLine(distancePoint, movingTarget.transform.position);
+            Gizmos.DrawLine(distancePoint, movingTarget.position);
         }
     }
 }
--- RonaldoBeltran3D/Assets/Scripts/Player/ControlledMovement.cs
+++ RonaldoBeltran3D/Assets/Scripts/Player/ControlledMovement.cs
@@ -6,19 +6,23 @@ public class ControlledMovement : MovScript
 {
     public float gravity;
     public CharacterController characterController;
+    Animator playerAnimator;
     float verticalSpeed;
     public float jumpforce = 10;
+    bool lastGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAnimator = transform.GetChild(0).GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (characterController.isGrounded)
+        RaycastHit info;
+        bool grounded = Physics.Raycast(transform.GetChild (0).position, Vector3.down, 0.25f);
+        if (grounded)
         {
             verticalSpeed -= gravity * Time.deltaTime;
         } else
@@ -28,12 +32,21 @@ public class ControlledMovement : MovScript
             {
                 Debug.Log (verticalSpeed);
                 verticalSpeed = jumpforce;
+                playerAnimator.SetTrigger("Jump");
                 Debug.Log (verticalSpeed);
             }
+            if (lastGrounded != characterController.isGrounded) { playerAnimator.SetTrigger("Land"); }
+            
         }
+        lastGrounded = characterController.isGrounded;
         Vector3 forwardAxis = transform.forward * speed * Input.GetAxis("Vertical");
         Vector3 verticalAxis = Vector3.down * verticalSpeed;
+        Vector3 horizontal = Vector3.up * Input.GetAxis("Horizontal");
 
         characterController.Move ((forwardAxis + verticalAxis) * Time.deltaTime);
+    }
+
+    void OnDrawGizmos () {
+
     }
 }
