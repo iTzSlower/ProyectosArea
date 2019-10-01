5119aea73fb75dd637f41e927b000696953f73b7
iTzslowerdtc <iTzslowerdtc@hotmail.com>
2019-09-30 21:55:21 -0500

    usando el Unity 2019

---
+++ Proyecto2DSI/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+    void Start()
+    {
+        if(target != null){
+
+            target.transform.parent = null;
+
+
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+        if (target != null)
+        {
+            float fixedSpeed = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, fixedSpeed);
+        }
+    }
+}
---
+++ Proyecto2DSI/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public void MoveToLevel(int index)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(index);
+    }
+}
